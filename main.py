f5d31a7419599a2c956e2c180ee84230d5597118
renanmd95 <renan.magalhaes@al.infnet.edu.br>
2024-08-09 12:07:08 +0000

    outro exemplo de concatenação de strings

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Tarefa para casa: reproduzir o programa com "Hello World!"
 print('Hello World!')
-print('Vasco da Gama, apenas.')
-print(2 + 4)
 
 nome = "Renan"
 sobrenome = "Magalhães"
 
 # Concatenando strings
 print(nome + " " + sobrenome)
+
+time = "Vasco da Gama"
+
+print(time + ", apenas.")
